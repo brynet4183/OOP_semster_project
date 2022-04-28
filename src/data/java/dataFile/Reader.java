734baec20e515021ab2c979d23e046f3c3f7fcae
jj <jj@hovmark.dk>
2022-04-28 16:38:33 +0200

    update2

---
+++ src/data/java/dataFile/Reader.java
@@ -0,0 +1,10 @@
+package dataFile;
+
+public class Reader {
+    private static String path = "";
+
+    public static String read(String file, int amount, int col, int intVal, String stingVal){
+
+        return null;
+    }
+}
---
+++ src/data/java/dataFile/Writer.java
@@ -0,0 +1,11 @@
+package dataFile;
+
+public class Writer {
+    private static String path = "";
+
+    public static void add(String file, int line, String stingVal){
+    }
+
+    public static void edit(String file, int line, String stingVal){
+    }
+}
