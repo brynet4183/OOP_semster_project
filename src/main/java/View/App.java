4d57e24dc519376cf00174b6322e41b0cd6b47a1
Trash-Child <dicte.mj.98@gmail.com>
2022-05-01 18:46:15 +0200

    Working towards scene change impl

--- src/main/java/View/App.java
+++ src/main/java/View/App.java
@@ -3,23 +3,35 @@ package View;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import java.io.IOException;
 
-
-
+//HUSK AT SPØRGE OM REGLERNE FOR BRUG AF BUDTZ' GIT REPO -
+// Hvordan erklærer vi det uden at blive sigtet for plagiat????
 public class App extends Application {
+    private Stage stage;
+
     public static void run(){
         launch();
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-    FXMLLoader loader = new FXMLLoader(getClass().getResource("/Controller.fxml"));
-        GridPane gridPane = loader.load();
-        Scene login = new Scene(gridPane);
+        this.stage = primaryStage;
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/LoginView.fxml"));
+        Scene login = loader.load();
+        LoginController controller = loader.getController();
         primaryStage.setScene(login);
-        primaryStage.show();
+        controller.setParentController(this);
+        stage.show();
+    }
+
+    public void login() throws IOException {
+
+    }
+    public void viewReigster() throws IOException{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/RegisterView.fxml"));
+            Scene scene = loader.load();
+            stage.setScene(scene);
     }
 }
