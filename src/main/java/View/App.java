f04a5f23d098417a1ec899fb653d7f7fc59d9e39
Trash-Child <dicte.mj.98@gmail.com>
2022-05-01 14:25:10 +0200

    base code setup before beginning work in Scene Builder

--- src/main/java/View/App.java
+++ src/main/java/View/App.java
@@ -1,4 +1,25 @@
 package View;
 
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+
+
+public class App extends Application {
+    public static void run(){
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/Controller.fxml"));
+        GridPane gridPane = loader.load();
+        Scene login = new Scene(gridPane);
+        primaryStage.setScene(login);
+        primaryStage.show();
+    }
 }
--- src/main/java/View/Runner.java
+++ src/main/java/View/Runner.java
@@ -1,4 +1,8 @@
 package View;
 
 public class Runner {
+    public static void main(String[] args) {
+        App.run();
+    }
+
 }
