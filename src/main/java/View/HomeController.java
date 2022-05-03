92ea602cf25a1b861273c91e14ae0e280f907a79
Trash-Child <dicte.mj.98@gmail.com>
2022-05-03 13:49:24 +0200

    wip

--- src/main/java/View/HomeController.java
+++ src/main/java/View/HomeController.java
@@ -1,5 +1,12 @@
 package View;
 
+import javafx.event.ActionEvent;
+
 public class HomeController {
 
+    public void volunteerGoToWorkplace(ActionEvent actionEvent) {
+    }
+
+    public void volunteerGoToHome(ActionEvent actionEvent) {
+    }
 }
