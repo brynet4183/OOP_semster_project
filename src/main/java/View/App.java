226f9b1c9e16c62f5b2c1364f0e31c04e47ff3cd
Trash-Child <dicte.mj.98@gmail.com>
2022-05-03 10:05:18 +0200

    Comments added to avoid plaigarism

--- src/main/java/View/App.java
+++ src/main/java/View/App.java
@@ -6,45 +6,45 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 
-//HUSK AT SPØRGE OM REGLERNE FOR BRUG AF BUDTZ' GIT REPO -
-// Hvordan erklærer vi det uden at blive sigtet for plagiat????
 public class App extends Application {
     private Stage stage;
-
     public static void run(){
         launch();
     }
 
     @Override
     public void start(Stage stage) throws Exception {
+        //Lånt fra Christian Budtz' GitHub
         this.stage = stage;
         goToLogin();
         stage.show();
     }
 
     public void login(int usr) throws IOException {
+        //Delementer lånt fra Christian Budtz' GitHub
         if (usr == 0) { //admin login
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/HomeView.fxml")); //TODO: change to admin view
             Scene scene = loader.load();
-
             stage.setScene(scene);
         }
         if (usr == 1){
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/HomeView.fxml")); //TODO: CHANGE TO VOL VIEW
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/VolunteerHomeView.fxml"));
             Scene scene = loader.load();
             stage.setScene(scene);
         }
     }
 
     public void goToRegister() throws IOException{
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/RegisterView.fxml"));
-            Scene scene = loader.load();
-            RegisterController controller = loader.getController();
-            controller.setParentController(this);
-            stage.setScene(scene);
+        //Lånt fra Christian Budtz' GitHub
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/RegisterView.fxml"));
+        Scene scene = loader.load();
+        RegisterController controller = loader.getController();
+        controller.setParentController(this);
+        stage.setScene(scene);
     }
 
     public void goToLogin() throws IOException {
+        //Lånt fra Christian Budtz' GitHub
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/LoginView.fxml"));
         Scene scene = loader.load();
         LoginController controller = loader.getController();
--- src/main/java/View/LoginController.java
+++ src/main/java/View/LoginController.java
@@ -14,38 +14,32 @@ import java.util.ResourceBundle;
 
 public class LoginController{
 
-    public Label underLoginLabel;
+    public Label loginStatusLabel;
     public TextField loginIDField;
     public PasswordField loginPasswordField;
     public RadioButton loginRadioVolunteer;
     public RadioButton loginRadioAdmin;
-    private App app;
 
+    //Struktur af reference til main-controller og funktionskald ved sceneskift
+    // (f.eks. login() eller goToRegister()) er lånt fra Christian Budtz' GitHub
+    private App app;
     public void setParentController(App app) {this.app = app;}
 
-    public void handleLoginBtnClick(ActionEvent actionEvent) {
-        try {
+    public void handleLoginBtnClick(ActionEvent actionEvent) throws IOException {
             if ( loginRadioAdmin.isSelected() &&
                     "admin".equals(loginIDField.getText()) &&
                     "admin".equals(loginPasswordField.getText())){
-                app.login(0);
+                app.login(0); //login mode: admin
             }
             else if (loginRadioVolunteer.isSelected()){ //TODO: currently has no check for credentials
-                app.login(1);
+                app.login(1); //login mode: volunteer
             }
-            else {underLoginLabel.setText("Forkert ID eller password");
-                  underLoginLabel.setTextFill(Color.RED);}
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            else {loginStatusLabel.setText("Forkert ID eller password");
+                  loginStatusLabel.setTextFill(Color.RED);}
     }
 
-    public void handleRegisterBtnClick(ActionEvent mouseEvent) {
-        try {
+    public void handleRegisterBtnClick(ActionEvent mouseEvent) throws IOException {
             app.goToRegister();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
 }
