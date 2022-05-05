c9dd87ed55156ef8377e03f23890924d82c1fee7
Trash-Child <dicte.mj.98@gmail.com>
2022-05-05 17:36:49 +0200

    all admin views corresponding to vol views done implemented passwordchange

--- src/main/java/Controller/ConfirmVolunteerController.java
+++ src/main/java/Controller/ConfirmVolunteerController.java
@@ -3,14 +3,19 @@ package Controller;
 import Main.App;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import static Main.App.context;
+import static Main.App.loginID;
+
 public class ConfirmVolunteerController implements Initializable {
 
 
+    public Label loggedInAsLabel;
     //Struktur af reference til main-controller, initialize og funktionskald ved sceneskift
     //er lånt fra Christian Budtz' GitHub
     private App app;
@@ -18,6 +23,7 @@ public class ConfirmVolunteerController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        loggedInAsLabel.setText("Admin " + context.TeamAdmins.get(loginID).personalInfo.getLastName());
 
     }
 
